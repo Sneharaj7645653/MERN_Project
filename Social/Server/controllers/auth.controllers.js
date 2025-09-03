edd109cb96457d720e6dc50161f6533d33d95ae8
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-09-03 14:44:00 +0530

    bug resolution

--- Social/Server/controllers/auth.controllers.js
+++ Social/Server/controllers/auth.controllers.js
@@ -1,51 +1,47 @@
+
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+
 export const signUp = async (req, res) => {
-    try {
-        console.log("Request body:", req.body);
-        const { name, userName, email, password } = req.body;
-
-
-        // Validate user data
-        if (!name || !userName || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        // Validate email 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "Email already in use" });
-        }
-
-        // Validate username
-        const existingUserName = await User.findOne({ userName });
-        if (existingUserName) {
-            return res.status(400).json({ message: "Username already taken" });
-        }
-
-        // password length
-        if (password.length < 6) {
-            return res
-                .status(400)
-                .json({ message: "Password must be at least 6 characters" });
-        }
-
-
-        // Hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        // Create new user
-        const newUser = new User({
-            name,
-            userName,
-            email,
-            hashedPassword,
-        });
-
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully" });
-
-    } catch (error) { 
-        res.status(500).json({ message: "Server error", error: error.message });
+  const { name, userName, email, password } = req.body;
+  console.log(req.body);
+  try {
+    // Validate User Data
+    if (!name || !userName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
+    // Validate Email
+
+    const existingUserEmail = await User.findOne({email});
+
+    if (existingUserEmail) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    // Validate for userName
+
+    const existingUserName = await User.findOne({ userName });
+
+    if (existingUserName) {
+      return res.status(400).json({ message: "Username already in use" });
+    }
+
+    // password length
+    if (password.length<=6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+   const hasedPassword=  await bcrypt.hash(password, salt);
+
+    // Create User
+
+     User.create({ name, userName, email, password: hasedPassword });
+    res.status(201).json({ message: "User created successfully"});
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
